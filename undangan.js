322b923c9b361b1eccdb6cfe5f9655e9606c1583
Munjin Nashida <miinjunn@gmail.com>
2023-10-18 03:07:04 +0700

    first commit: main file for sense invitation project

---
+++ undangan.js
@@ -0,0 +1,54 @@
+document.getElementById("next").onclick = function () {
+  let lists = document.querySelectorAll(".item");
+  document.getElementById("slide").appendChild(lists[0]);
+};
+document.getElementById("prev").onclick = function () {
+  let lists = document.querySelectorAll(".item");
+  document.getElementById("slide").prepend(lists[lists.length - 1]);
+};
+
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  let today = new Date(),
+    dd = String(today.getDate()).padStart(2, "0"),
+    mm = String(today.getMonth() + 1).padStart(2, "0"),
+    yyyy = today.getFullYear(),
+    nextYear = yyyy + 0,
+    month_Day = "12/03/",
+    birthday = month_Day + yyyy;
+
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = month_Day + nextYear;
+  }
+
+  const countDown = new Date(birthday).getTime(),
+    x = setInterval(function () {
+      const now = new Date().getTime(),
+        distance = countDown - now;
+
+      (document.getElementById("days").innerText = Math.floor(distance / day)),
+        (document.getElementById("hours").innerText = Math.floor(
+          (distance % day) / hour
+        )),
+        (document.getElementById("minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.getElementById("seconds").innerText = Math.floor(
+          (distance % minute) / second
+        ));
+
+      //do something later when date is reached
+      if (distance < 0) {
+        document.getElementById("headline").innerText = "It's my birthday!";
+        document.getElementById("countdown").style.display = "none";
+        document.getElementById("content").style.display = "block";
+        clearInterval(x);
+      }
+      //seconds
+    }, 0);
+})();
